0b4aaf124757539aea3b566311761ac88820a7bd
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2024-04-29 06:27:23 +0000

    First Commit

---
+++ ne5/ne5/doctype/merchants/merchants.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Yash Wadgaonkar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Merchants", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ne5/ne5/doctype/merchants/test_merchants.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Yash Wadgaonkar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMerchants(FrappeTestCase):
+	pass
